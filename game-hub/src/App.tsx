5da9045c1776afc5c51ddc7765dc7438ef308113
Teerath Bajaj <teerath.bajaj24@gmail.com>
2023-11-06 11:44:02 +0900

    build a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,5 +1,18 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid templateAreas={`"nav nav" "aside main"`}>
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 export default App;
