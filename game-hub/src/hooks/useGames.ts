0798b109c7d62d381ae146708ac857836df50b83
Teerath Bajaj <teerath.bajaj24@gmail.com>
2023-11-08 12:29:54 +0900

    show loading skepetons

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -25,20 +25,28 @@ const useGames = () => {
     //useState varible for storing the game objects
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   //effect hook to send a fetch request to the backend
   useEffect(() => {
-    const controller = new AbortController()
+    const controller = new AbortController();
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results)) //use typescript to define an interface that reps the shape of the response object
+      .then((res) => {
+      setGames(res.data.results);
+      setLoading(false);
+      }) //use typescript to define an interface that reps the shape of the response object
       .catch((err) => {
         if(err instanceof CanceledError) return 
-        setError(err.message)});
+        setError(err.message)
+        setLoading(false); 
+      });
+
       return() => controller.abort();
   }, []);
 
-  return{games, error};
+  return{games, error, isLoading};
 }
 
 export default useGames
