e8937a74e9ba28a21ae5c429e21bb149d59a8d68
Teerath Bajaj <teerath.bajaj24@gmail.com>
2023-11-08 12:41:29 +0900

    Refractor: remove duplicated styles

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card>
       <Image src={getCroppedImageURL(game.background_image)} />
       <CardBody>
         <Heading fontSize="19px">{game.name}</Heading>
