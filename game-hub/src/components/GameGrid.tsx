e998baff135b6dbfb15418b24c4a6b2129480cf4
Teerath Bajaj <teerath.bajaj24@gmail.com>
2023-11-08 19:57:07 +0900

    Displaying genres

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -6,15 +6,11 @@ import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="15px"
-        spacing={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="15px" spacing={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
